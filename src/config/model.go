a2c1b31f4c2c96f825a9d76cab6e780747d325fd
Maximilian Wittenburg <max.wittenburg@gmail.com>
2023-01-18 21:41:11 +0100

    delete no longer needed model

--- src/config/model.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type DotlanConfig struct {
-	UserId           uint   `json:"dotlanUserId"`
-	TournamentFilter string `json:"dotlanTournamentFilter"`
-	DefaultGame      string `json:"defaultGame"`
-}
